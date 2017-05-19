c16b39ec694148b46b52610737548dc236352fd3
saranpong <saranpong@bangkokair.com>
2017-05-19 12:43:38 +0700

    first init

---
+++ TestWindowsService/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWindowsService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(string message)
+        {
+            StreamWriter sw  = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFiles.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
